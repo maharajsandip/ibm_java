69eafec477bf5e3365cda968d6e5b645eda96fe8
Sandip Maharaj <maharaj_sandip@berkeley.edu>
2026-03-04 15:34:23 -0500

    Complete explore data types lab

---
+++ my_datatypes_proj/src/DataCharToInt.java
@@ -0,0 +1,9 @@
+public class DataCharToInt {
+    public static void main(String s[]) {
+        char letter = 'a';
+        System.out.println("char: " + letter);
+
+        int letter_int = (int)letter;
+        System.out.println("ASCII value of a id: " + letter_int);
+    }
+}
