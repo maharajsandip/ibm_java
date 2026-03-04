02509a3bc1c232d950fb152e042eb2451d96c132
Sandip Maharaj <maharaj_sandip@berkeley.edu>
2026-03-04 14:59:21 -0500

    complete create your first class lab

---
+++ my_class_proj/src/BooksMenu.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class BooksMenu {
+    /**
+     * determine the more expensive book
+     * @param book1 price of book 1
+     * @param book2 price of book 2
+     * @return book that has the higher price
+     */
+    private static Book getExpensiveBook(Book book1, Book book2) {
+        if (book1.getPrice() > book2.getPrice()) {
+            return book1;
+        } else {
+            return book2;
+        }
+    }
+	public static void main(String s[]) {
+		Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+		Book[] books = new Book[10];
+		int bkIdx = 0;
+
+		while(true) {
+			System.out.println("Press 1 to view books\n" +
+                                "2 to add books\n" +
+                                "3 to compare book prices\n" +
+                                "any other key to exit");
+			String userAction = scanner.nextLine();
+
+			if (userAction.equals("1")) {
+				for(int i = 0; i < books.length; i++) {
+					if(books[i] != null) {
+						System.out.println(books[i]);
+					}
+				}
+			} else if (userAction.equals("2")) {
+				if(bkIdx == 10) {
+					System.out.println("10 books added already. Cannot add any more books!");
+					continue;
+				}
+
+				System.out.println("Enter book title");
+				String tmpTitle = scanner.nextLine();
+
+				System.out.println("Enter book author");
+				String tmpAuthor = scanner.nextLine();
+
+				System.out.println("Enter book price");
+				float tmpPrice = Float.parseFloat(scanner.nextLine());
+
+				Book bkTmp = new Book();
+				bkTmp.setTitle(tmpTitle);
+				bkTmp.setAuthor(tmpAuthor);
+				bkTmp.setPrice(tmpPrice);
+
+				books[bkIdx++] = bkTmp;
+			} else if (userAction.equals("3")) {
+                System.out.println("enter index of the first book to compare");
+                int bkIdx_1 = Integer.parseInt(scanner.nextLine());
+
+                System.out.println("enter index of the second book to compare");
+                int bkIdx_2 = Integer.parseInt(scanner.nextLine());
+
+                if (books[bkIdx_2] != null && books[bkIdx_2] != null) {
+                    System.out.println("Details of the expensive book is\n" + 
+                                        getExpensiveBook(books[bkIdx_1], books[bkIdx_2]));
+                } else {
+                    System.out.println("One of the books is null");
+                }
+            } else {
+                scanner.close();
+				break;
+			}
+		}
+	}
+}
